036233b17cc9bcf620b77f3093ce104ea3574eaa
Priyanshu sahu <Prayanshu771@gmail.com>
2023-09-13 14:12:32 +0530

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   config.asset_host = "http://103.12.1.81/"
+  #  config.asset_host = "http://103.12.1.81/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
