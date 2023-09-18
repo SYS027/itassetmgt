0fcb5a75c25e98786b646a2d7cd755481b946fb0
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-18 19:02:58 +0530

    Q12

--- db/migrate/20230811131040_create_assets.rb
+++ db/migrate/20230811131040_create_assets.rb
@@ -1,7 +1,7 @@
 class CreateAssets < ActiveRecord::Migration[7.0]
   def change
     create_table :assets do |t|
-      t.references :company, null: false, foreign_key: true
+      t.references :companyID, null: false, foreign_key: true
       t.references :product_category, null: false, foreign_key: true
       t.references :product_type, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
