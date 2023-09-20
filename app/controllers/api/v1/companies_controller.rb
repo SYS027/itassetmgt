85d22214e296e9cb52139311f84d7e2c1ae0625a
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-20 17:21:06 +0530

    v1

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -19,8 +19,16 @@ class Api::V1::CompaniesController < ApplicationController
   def company_params
     params.require(:company).permit(:name, :email, :contact_number, :portal_name, :industry, :number_of_employees, :tax_information, :country_id, :state_id, :city_id, :pin_code, :address, :logo)
   end
-
+  
   def current_admin_id
+    # Assuming you can access the current admin using admin.current
+    # Replace 'Admin' with the actual model name for admin users
+    admin = Admin.current
+    return admin.id if admin.present?
+    # Handle the case where there is no current admin
+    # You can return nil or handle it as needed
+    # For simplicity, I'll return nil here.
+    nil
   end
   
 end
