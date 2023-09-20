79881c3cb0c0df99f1355f2de8503fda34075a24
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-20 12:20:06 +0530

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins [
-        "http://localhost:3001",
+        "http://localhost:3000",
         "https://itassetmgt-7659d44ac774.herokuapp.com",
         "https://itassetmgt-7659d44ac774.herokuapp.com/api/v1",
     ]
