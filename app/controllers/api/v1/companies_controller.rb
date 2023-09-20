9b0e26d6f48aa5da68c8242ab139f308210e81e1
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-20 16:27:54 +0530

    company

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -8,13 +8,14 @@ class Api::V1::CompaniesController < ApplicationController
     @company.admin_id = 1
 
     if @company.save
-      render json: @company, status: :createdD
+      render json: @company, status: :created
     else
       render json: { errors: @company.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
+
   def company_params
     params.require(:company).permit(:name, :email, :contact_number, :portal_name, :industry, :number_of_employees, :tax_information, :country_id, :state_id, :city_id, :pin_code, :address, :logo)
   end
