db5428dba372e55cbe8c05fd0af3f1634826ef7d
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-20 17:00:38 +0530

    update current admin

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::CompaniesController < ApplicationController
 
   def create
     @company = Company.new(company_params)
-    @company.admin_id = 1
+    @company.admin_id = current_admin_id # Set the admin_id to the current admin's ID
 
     if @company.save
       render json: @company, status: :created
@@ -19,4 +19,21 @@ class Api::V1::CompaniesController < ApplicationController
   def company_params
     params.require(:company).permit(:name, :email, :contact_number, :portal_name, :industry, :number_of_employees, :tax_information, :country_id, :state_id, :city_id, :pin_code, :address, :logo)
   end
+
+  def current_admin_id
+    # Assuming you store the admin's ID in the authentication token
+    # Replace 'YOUR_AUTH_TOKEN_KEY' with the actual key used in your token
+    token = request.headers['Authorization']
+    if token.present?
+      decoded_token = JWT.decode(token, 'YOUR_SECRET_KEY', true, algorithm: 'HS256')
+      admin_id = decoded_token[0]['admin_id']
+      return admin_id
+    else
+      # Handle the case where there is no token or it's invalid
+      # You can return an error, raise an exception, or handle it as needed
+      # For simplicity, I'll return nil here.
+      return nil
+    end
+  end
+  
 end
