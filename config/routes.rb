9ff4e232e2451f664e86e7499b8cc01653fdde1c
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-20 16:35:16 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,19 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :departments
-      resources :locations
-      resources :profiles
-      resources :asset
       devise_for :admins, controllers: {
         sessions: 'api/v1/sessions',
         registrations: 'api/v1/registrations',
         password_resets: 'api/v1/password_resets'
       }
-      get 'productcategories',to: 'productcategories#index'
-      get 'producttype',to: 'producttype#index'
-      post 'product/create', to: 'product#create'
-      get 'products', to: 'product#index'
-      delete 'product/destroy/:id', to: 'product#destroy'
-      put 'product/:id', to: 'product#update'
-      get 'products/:id', to: 'product#show'
-     
       resources :registrations, only: [:index, :create]
-      resources :products
       resources :sessions, only: [:create]
       resources :companies
+      resources :departments
+      resources :locations
+      resources :profiles
+      resources :asset
+      resources :products
       resources :countries
       resources :states
       resources :cities
@@ -34,6 +26,13 @@ Rails.application.routes.draw do
       resources :purchase_types
       resources :assign_assets
       resources :totalcost_assets
+      get 'productcategories',to: 'productcategories#index'
+      get 'producttype',to: 'producttype#index'
+      post 'product/create', to: 'product#create'
+      get 'products', to: 'product#index'
+      delete 'product/destroy/:id', to: 'product#destroy'
+      put 'product/:id', to: 'product#update'
+      get 'products/:id', to: 'product#show'
       get 'vendors/total_count', to: 'vendors#total_count'
       delete 'logout', to: 'sessions#destroy'
       get "password_resets", to: 'password_resets#index'
