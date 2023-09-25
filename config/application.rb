6b57f42ff4577aec78d37f11c76d4a4b35858c0a
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-25 14:25:18 +0530

    update for cors

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,14 @@
-require_relative "boot"
-require "rails/all"
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-Bundler.require(*Rails.groups)
-
-module AssetManager
-  class Application < Rails::Application
-    config.load_defaults 7.0
-
-    # Uncomment the following line to enable sessions
-    config.api_only = false
-
-    # Uncomment the following line if you want to use cookies for sessions
-    config.session_store :cookie_store, key: '_asset_manager_session'
-
-    # Uncomment the following line if you want to use the database for sessions
-    # config.session_store :active_record_store, :key => '_asset_manager_session'
-
-    # Configuration for the application, engines, and railties goes here.
-    # ...
-  end
-end
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://itassetmgt-7659d44ac774.herokuapp.com',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '', // Remove the '/api' prefix from the request path
+      },
+    })
+  );
+};
