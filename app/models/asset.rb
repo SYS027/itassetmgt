9aedbea9f8ff665b3564643457b3f222f5ed6e68
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-25 12:27:50 +0530

    update asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,7 +5,7 @@ class Asset < ApplicationRecord
   belongs_to :product
   belongs_to :location
   belongs_to :purchase_type
-  belongs_to :asset_specification
+  belongs_to :asset_specification, optional: true
   belongs_to :vendor
   has_one :assign_assets
 end
