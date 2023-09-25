9e97d45361aeb1fc294965a187a85e887e6a2f73
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-25 14:29:38 +0530

    update again cors

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,28 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+require_relative "boot"
+require "rails/all"
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://itassetmgt-7659d44ac774.herokuapp.com',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '', // Remove the '/api' prefix from the request path
-      },
-    })
-  );
-};
+Bundler.require(*Rails.groups)
+module AssetManager
+  class Application < Rails::Application
+    config.load_defaults 7.0
+
+    # Enable sessions and cookies for the application
+    config.api_only = false
+
+    # Use the cookie_store for session management
+    config.session_store :cookie_store, key: '_asset_manager_session'
+
+    # CORS Configuration to allow requests from all origins
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://apps.itassetmgt.com'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+    
+
+    # Configuration for the application, engines, and railties goes here.
+    # ...
+  end
+end
+
