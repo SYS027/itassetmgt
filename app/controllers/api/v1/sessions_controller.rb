e6a7984b625349f9599370f4f40134a53df8e91e
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-25 16:27:09 +0530

    update

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,25 +2,35 @@ class Api::V1::SessionsController < ApplicationController
   before_action :authenticate_admin, only: [:destroy]
 
   def create
-    admin = Admin.find_by(email: params[:email])
-    if admin&.authenticate(params[:password])
-      session[:admin_id] = admin.id
-      render json: { message: 'Login successful', admin_id: admin.id }, status: :ok
+    auth_token = params[:admin][:auth_token]
+    email = params[:admin][:email]
+    password = params[:admin][:password]
+    if auth_token.present?
+      admin = Admin.find_by(auth_token: auth_token)
+      if admin
+        sign_in(admin)
+        render json: { message: 'Login successful', admin_id: admin.id, auth_token: admin.auth_token }, status: :ok
+      else
+        authenticate_with_email_and_password(email, password)
+      end
     else
-      render json: { message: 'Invalid email or password' }, status: :unauthorized
+      authenticate_with_email_and_password(email, password)
     end
   end
-
   def destroy
-    session[:admin_id] = nil
+    sign_out(current_admin) if current_admin
     render json: { message: 'Logout successful' }, status: :ok
   end
-
+  
   private
-
-  def authenticate_admin
-    unless current_admin
-      render json: { message: 'Not authenticated' }, status: :unauthorized
+  def authenticate_with_email_and_password(email, password)
+    admin = Admin.find_by(email: email)
+    if admin&.valid_password?(password)
+      sign_in(admin)
+      render json: { message: 'Login successful', admin_id: admin.id, auth_token: admin.auth_token }, status: :ok
+    else
+      render json: { message: 'Invalid credentials' }, status: :unauthorized
     end
   end
 end
+
