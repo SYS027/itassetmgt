eeb03815ba5ab66de04f3d484317d16c50e4f747
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 12:41:01 +0530

    sdsd

--- db/migrate/20230811132032_remove_asset_table_and_add_asset_to_assign_assets.rb
+++ db/migrate/20230811132032_remove_asset_table_and_add_asset_to_assign_assets.rb
@@ -3,10 +3,10 @@ class RemoveAssetTableAndAddAssetToAssignAssets < ActiveRecord::Migration[7.0]
     # First, remove the existing foreign key constraint
     remove_reference :assign_assets, :asset_table, null: false, foreign_key: true
 
-    # Change the data type of 'asset_id' to UUID
-    change_column :assign_assets, :asset_id, :uuid
+    # Add a new 'asset_id' column with the UUID data type
+    add_column :assign_assets, :asset_id, :uuid
 
-    # Add the new foreign key constraint to 'asset'
-    add_reference :assign_assets, :asset, null: false, foreign_key: true
+    # Add a foreign key constraint to the 'asset' table
+    add_foreign_key :assign_assets, :assets, column: :asset_id, on_delete: :cascade
   end
 end
