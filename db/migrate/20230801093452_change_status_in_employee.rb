f6f008fd546964666d50e499579957c54f1580db
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:32:07 +0530

    xcvxc

--- db/migrate/20230801093452_change_status_in_employee.rb
+++ db/migrate/20230801093452_change_status_in_employee.rb
@@ -1,10 +1,5 @@
-class ChangeStatusInEmployee < ActiveRecord::Migration[7.0]
+class ChangeStatusInEmployeess < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto') # Add this line if you want to use UUIDs
-
-    rename_column :employees, :Location, :location
-    rename_column :employees, :Reporting_manager, :reporting_manager
-    rename_column :employees, :Deparment, :department
-    add_column :employees, :status, :boolean
+    change_column_default :employees, :status, true
   end
 end
