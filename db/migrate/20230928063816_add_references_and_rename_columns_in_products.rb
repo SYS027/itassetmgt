636b5023291ac5207f2a9e8a0476d77f4f14fa84
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 12:54:15 +0530

    dsds

--- db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
+++ db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
@@ -1,7 +1,14 @@
 class AddReferencesAndRenameColumnsInProducts < ActiveRecord::Migration[7.0]
   def change
+    # First, remove the existing foreign key constraints
+    remove_foreign_key :products, column: :product_category_id
+    remove_foreign_key :products, column: :product_type_id
+
+    # Then, add the new references with the correct datatype
     add_reference :products, :product_category, foreign_key: { to_table: :product_categories, type: :uuid }
     add_reference :products, :product_type, foreign_key: { to_table: :product_types, type: :uuid }
+
+    # Finally, rename the columns
     rename_column :products, :Product_Name, :product_name
     rename_column :products, :Manufacturer, :manufacturer
   end
