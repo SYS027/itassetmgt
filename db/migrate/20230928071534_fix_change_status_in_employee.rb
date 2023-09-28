3f860a65bd6b778be9ba1b10e5347f7c36e9c774
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 16:23:15 +0530

    dasdas

--- db/migrate/20230928071534_fix_change_status_in_employee.rb
+++ db/migrate/20230928071534_fix_change_status_in_employee.rb
@@ -1,9 +1,9 @@
 class FixChangeStatusInEmployee < ActiveRecord::Migration[7.0]
   def change
     # Modify the existing migration code here to correct any issues
-    rename_column :employees, :Location, :location
-    rename_column :employees, :Reporting_manager, :reporting_manager
-    rename_column :employees, :Deparment, :department
+    # rename_column :employees, :Location, :location
+    # rename_column :employees, :Reporting_manager, :reporting_manager
+    # rename_column :employees, :Deparment, :department
     add_column :employees, :status, :boolean
   end
 end
