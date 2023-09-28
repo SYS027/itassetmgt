ce1f625b6bc97f3a8f1886c7f4e48eb697c3f24c
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:45:01 +0530

    dfgfdg

--- db/migrate/20230928061437_change_status_in_employeess.rb
+++ db/migrate/20230928061437_change_status_in_employeess.rb
@@ -1,9 +1,5 @@
 class ChangeStatusInEmployeess < ActiveRecord::Migration[7.0]
-  def up
+  def change
     change_column_default :employees, :status, true
-  end
-
-  def down
-    change_column_default :employees, :status, nil
   end
 end
