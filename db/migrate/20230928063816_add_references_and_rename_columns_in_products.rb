e5c7b581b30a05f850bd9fc349b812f43127c048
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 13:01:20 +0530

    sdd

--- db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
+++ db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
@@ -1,9 +1,7 @@
 class AddReferencesAndRenameColumnsInProducts < ActiveRecord::Migration[7.0]
-  def change
-    # Check if an existing foreign key constraint exists and remove it
-    if foreign_key_exists?(:products, :product_category_id)
-      remove_foreign_key :products, :product_category_id
-    end
+  def up
+    # Change the datatype of product_category_id to uuid
+    change_column :products, :product_category_id, :uuid, using: 'product_category_id::uuid'
 
     # Add the new reference with the correct datatype
     add_reference :products, :product_category, foreign_key: { to_table: :product_categories, type: :uuid }
@@ -12,5 +10,16 @@ class AddReferencesAndRenameColumnsInProducts < ActiveRecord::Migration[7.0]
     # Rename the columns
     rename_column :products, :Product_Name, :product_name
     rename_column :products, :Manufacturer, :manufacturer
+  end
+
+  def down
+    # Revert the changes in reverse order if needed
+    rename_column :products, :manufacturer, :Manufacturer
+    rename_column :products, :product_name, :Product_Name
+
+    remove_reference :products, :product_type, foreign_key: true
+    remove_reference :products, :product_category, foreign_key: true
+
+    change_column :products, :product_category_id, :bigint
   end
 end
