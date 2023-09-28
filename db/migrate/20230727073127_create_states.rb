02fc81c61c4a82c7079a29f1cc424399e2073ad7
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:03:27 +0530

    update state

--- db/migrate/20230727073127_create_states.rb
+++ db/migrate/20230727073127_create_states.rb
@@ -4,9 +4,11 @@ class CreateStates < ActiveRecord::Migration[7.0]
 
     create_table :states, id: :uuid do |t|
       t.string :state_name
-      t.references :country, null: false, foreign_key: { to_table: :countries, type: :uuid }
-
+      t.uuid :country_id, null: false  # Change the data type to uuid
       t.timestamps
     end
+
+    # Add a foreign key constraint with matching data types
+    add_foreign_key :states, :countries, column: :country_id, type: :uuid
   end
 end
