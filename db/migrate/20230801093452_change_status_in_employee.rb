922a7c2479618446a228a675aa915bf1f89c9ab3
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:33:26 +0530

    aaa

--- db/migrate/20230801093452_change_status_in_employee.rb
+++ db/migrate/20230801093452_change_status_in_employee.rb
@@ -1,5 +1,8 @@
-class ChangeStatusInEmployeess < ActiveRecord::Migration[7.0]
+class ChangeStatusInEmployee < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :employees, :status, true
+    rename_column :employees , :Location , :location
+    rename_column :employees , :Reporting_manager , :reporting_manager
+    rename_column :employees , :Deparment , :department
+    add_column :employees , :status, :boolean
   end
 end
