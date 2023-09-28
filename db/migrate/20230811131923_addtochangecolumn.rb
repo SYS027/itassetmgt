a1ff99cc02fb9a31ce5607496a1dde96d24aef21
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 12:32:08 +0530

    ssss

--- db/migrate/20230811131923_addtochangecolumn.rb
+++ db/migrate/20230811131923_addtochangecolumn.rb
@@ -1,7 +1,9 @@
 class Addtochangecolumn < ActiveRecord::Migration[7.0]
   def change
     # First, remove the existing columns
-    remove_column :employees, :location_id
+    if column_exists?(:employees, :location_id)
+      remove_column :employees, :location_id
+    end
     remove_column :employees, :department_id
 
     # Then, add the references columns
