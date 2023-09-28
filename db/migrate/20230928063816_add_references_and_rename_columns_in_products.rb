6d89a8cb3664df2b1d088c678b38752feb3e0e24
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 15:14:07 +0530

    xdsa

--- db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
+++ db/migrate/20230928063816_add_references_and_rename_columns_in_products.rb
@@ -1,25 +1,8 @@
 class AddReferencesAndRenameColumnsInProducts < ActiveRecord::Migration[7.0]
-  def up
-    # Change the datatype of product_category_id to uuid
-    # change_column :products, :product_category_id, :uuid, using: 'product_category_id::uuid'
-
-    # Add the new reference with the correct datatype
+  def change
     add_reference :products, :product_category, foreign_key: { to_table: :product_categories, type: :uuid }
     add_reference :products, :product_type, foreign_key: { to_table: :product_types, type: :uuid }
-
-    # Rename the columns
     rename_column :products, :Product_Name, :product_name
     rename_column :products, :Manufacturer, :manufacturer
-  end
-
-  def down
-    # Revert the changes in reverse order if needed
-    rename_column :products, :manufacturer, :Manufacturer
-    rename_column :products, :product_name, :Product_Name
-
-    remove_reference :products, :product_type, foreign_key: true
-    remove_reference :products, :product_category, foreign_key: true
-
-    change_column :products, :product_category_id, :bigint
   end
 end
