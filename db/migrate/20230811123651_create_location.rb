6e231a826a7cf386034cb91953b77b3d67945d6c
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:48:43 +0530

    hfghfg

--- db/migrate/20230811123651_create_location.rb
+++ db/migrate/20230811123651_create_location.rb
@@ -1,21 +1,17 @@
-class CreateLocations < ActiveRecord::Migration[7.0]
+class CreateLocation < ActiveRecord::Migration[7.0]
 	def change
-	  enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-  
-	  create_table :locations, id: :uuid do |t|
-		t.references :company, null: false, foreign_key: true, type: :uuid
-		t.string :office_name
-		t.string :poc_name
-		t.string :poc_email
-		t.string :poc_contact
-		t.references :country, null: false, foreign_key: true, type: :uuid
-		t.references :state, null: false, foreign_key: true, type: :uuid
-		t.references :city, null: false, foreign_key: true, type: :uuid
-		t.string :zip_code
-		t.boolean :is_active
-  
+	  create_table :locations do |t|
+		  t.references :company, null: false, foreign_key: true
+		  t.string :office_name
+		  t.string :poc_name
+		  t.string :poc_email
+		  t.string :poc_contact
+		  t.references :country, null: false, foreign_key: true
+		  t.references :state, null: false, foreign_key: true
+		  t.references :city, null: false, foreign_key: true
+		  t.string :zip_code
+		  t.boolean :is_active
 		t.timestamps
 	  end
 	end
   end
-  
