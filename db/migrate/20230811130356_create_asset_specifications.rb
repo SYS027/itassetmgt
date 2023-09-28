a0f98d9a4a6fdd444fbcd894927108d6b39a0805
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 12:14:27 +0530

    assa

--- db/migrate/20230811130356_create_asset_specifications.rb
+++ db/migrate/20230811130356_create_asset_specifications.rb
@@ -1,7 +1,9 @@
 class CreateAssetSpecifications < ActiveRecord::Migration[7.0]
   def change
-    create_table :asset_specifications do |t|
-      t.references :company, null: false, foreign_key: true
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+
+    create_table :asset_specifications, id: :uuid do |t|
+      t.references :company, null: false, foreign_key: true, type: :uuid
       t.string :name
       t.string :value
       t.boolean :is_active, null: false, default: true
