9cb5d8e8a31b0af30104feb222d4824945d457cb
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 12:25:07 +0530

    sasd

--- db/migrate/20230811131612_add_company_id_to_department.rb
+++ db/migrate/20230811131612_add_company_id_to_department.rb
@@ -1,5 +1,5 @@
 class AddCompanyIdToDepartment < ActiveRecord::Migration[7.0]
   def change
-    add_reference :departments, :company, foreign_key: true
+    add_reference :departments, :company, foreign_key: true, type: :uuid
   end
 end
