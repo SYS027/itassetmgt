af02e26c9e6e2ca87460a10e4bdb03645c1ccb6f
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:18:30 +0530

    zzzz

--- db/migrate/20230727073146_create_cities.rb
+++ db/migrate/20230727073146_create_cities.rb
@@ -1,15 +1,10 @@
 class CreateCities < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-
     create_table :cities, id: :uuid do |t|
-      t.string :name, null: false
-      t.references :country, null: false, foreign_key: { to_table: :countries, type: :uuid }
-      t.references :state, null: false, foreign_key: { to_table: :states, type: :uuid }
+      t.string :name
+      t.references :country, type: :uuid, foreign_key: true
 
       t.timestamps
     end
-
-    add_index :cities, :name, unique: true # Add a unique index on the name column
   end
 end
