3c9a1ae91d8a44ea64c84640b6bdc09e5690f8b6
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 10:46:41 +0530

    a123

--- db/migrate/20230727073127_create_states.rb
+++ db/migrate/20230727073127_create_states.rb
@@ -1,8 +1,10 @@
 class CreateStates < ActiveRecord::Migration[7.0]
   def change
-    create_table :states do |t|
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+
+    create_table :states, id: :uuid do |t|
       t.string :state_name
-      t.references :country, null: false, foreign_key: true
+      t.references :country, null: false, foreign_key: { to_table: :countries, type: :uuid }
 
       t.timestamps
     end
