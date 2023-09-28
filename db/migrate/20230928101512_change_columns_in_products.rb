1e4d4116eeba43e97eadbefac8f8e46ce8548e0c
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 16:30:26 +0530

    sdsd

--- db/migrate/20230928101512_change_columns_in_products.rb
+++ db/migrate/20230928101512_change_columns_in_products.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsInProducts < ActiveRecord::Migration[7.0]
 	def change
-		remove_column :products, :product_category_id
+		# remove_column :products, :product_catagory_id
 		remove_column :products, :product_type_id
 		add_reference :products, :product_category, foreign_key: true , type: :uuid 
 		add_reference :products, :product_type, foreign_key: true , type: :uuid 
