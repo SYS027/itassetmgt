ca98d07547ced0c5cfe82cd82286dfcade28a4c0
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 16:49:51 +0530

    d

--- db/migrate/20230928111501_add_product_category_id_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddProductCategoryIdToProducts < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :ProductCategory., :product_category_id, null: false, foreign_key: true , type: :uuid
-  end
-end
