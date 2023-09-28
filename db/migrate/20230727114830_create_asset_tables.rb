42ab5056f9c372f1b0b47eccbe2d20a18f4d35f4
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:23:22 +0530

    asad

--- db/migrate/20230727114830_create_asset_tables.rb
+++ db/migrate/20230727114830_create_asset_tables.rb
@@ -3,27 +3,35 @@ class CreateAssetTables < ActiveRecord::Migration[7.0]
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
     create_table :asset_tables, id: :uuid do |t|
-      t.references :company, foreign_key: true, type: :uuid
+      t.references :company, type: :uuid
       t.integer :asset_id
-      t.references :product_category, foreign_key: true, type: :uuid
-      t.references :product_type, foreign_key: true, type: :uuid
+      t.references :product_category, type: :uuid
+      t.references :product_type, type: :uuid
       t.integer :product_id
-      t.references :vendor, foreign_key: true, type: :uuid
+      t.references :vendor, type: :uuid
       t.string :asset_name
       t.integer :price
       t.string :description_id
-      t.references :company_location, foreign_key: true, type: :uuid
+      t.references :company_location, type: :uuid
       t.date :purchase_date
       t.date :warranty_expiry_date
-      t.references :purchase_type, foreign_key: true, type: :uuid
+      t.references :purchase_type, type: :uuid
       t.integer :useful_life
       t.integer :residual_value
       t.integer :description
-      t.references :asset_specification, foreign_key: true, type: :uuid
+      t.references :asset_specification, type: :uuid
       t.boolean :is_active
 
       t.timestamps
     end
+
+    add_foreign_key :asset_tables, :companies, column: :company_id
+    add_foreign_key :asset_tables, :product_categories, column: :product_category_id
+    add_foreign_key :asset_tables, :product_types, column: :product_type_id
+    add_foreign_key :asset_tables, :vendors, column: :vendor_id
+    add_foreign_key :asset_tables, :company_locations, column: :company_location_id
+    add_foreign_key :asset_tables, :purchase_types, column: :purchase_type_id
+    add_foreign_key :asset_tables, :asset_specifications, column: :asset_specification_id
   end
 
   def down
