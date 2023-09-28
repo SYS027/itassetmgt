de864550d55a3e107ed8a9425a323d2fd736e57f
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 15:50:03 +0530

    dsasadsd

--- db/migrate/20230928070439_addtochangecolumn.rb
+++ db/migrate/20230928070439_addtochangecolumn.rb
@@ -1,13 +1,8 @@
 class Addtochangecolumn < ActiveRecord::Migration[7.0]
   def change
-    # First, remove the existing columns
-    if column_exists?(:employees, :location_id)
-      remove_column :employees, :location_id
-    end
-    remove_column :employees, :department_id
-
-    # Then, add the references columns
-    add_reference :employees, :location, foreign_key: true, type: :uuid
-    add_reference :employees, :department, foreign_key: true, type: :uuid
+    remove_column :employees, :location
+    remove_column :employees, :department
+    add_reference :employees, :location, foreign_key: true
+    add_reference :employees, :department, foreign_key: true
   end
 end
