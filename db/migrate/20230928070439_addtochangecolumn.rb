9eeb51b2465059e2db1e269dbb2228472f990873
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 16:18:31 +0530

    sfsd

--- db/migrate/20230928070439_addtochangecolumn.rb
+++ db/migrate/20230928070439_addtochangecolumn.rb
@@ -2,7 +2,7 @@ class Addtochangecolumn < ActiveRecord::Migration[7.0]
   def change
     remove_column :employees, :location
     remove_column :employees, :department
-    add_reference :employees, :location, foreign_key: { to_table: :employees, type: :uuid }
-    add_reference :employees, :department, foreign_key: { to_table: :employees, type: :uuid }
+    add_reference :employees, :location, foreign_key: true , type: :uuid 
+    add_reference :employees, :department, foreign_key: true , type: :uuid 
   end
 end
