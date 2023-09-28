6c0b293a59af220203b110eee53a9e7ee4cf2078
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-28 11:28:47 +0530

    cvbn

--- db/migrate/20230727114830_create_asset_tables.rb
+++ db/migrate/20230727114830_create_asset_tables.rb
@@ -1,34 +1,26 @@
 class CreateAssetTables < ActiveRecord::Migration[7.0]
-  def up
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-
-    create_table :asset_tables, id: :uuid do |t|
-      t.references :company, foreign_key: true, type: :uuid
+  def change
+    create_table :asset_tables do |t|
+      t.integer :companyId
       t.integer :asset_id
-      t.references :product_category, foreign_key: true, type: :uuid
-      t.references :product_type, foreign_key: true, type: :uuid
+      t.integer :product_catagory_id
+      t.integer :product_type_id
       t.integer :product_id
-      t.references :vendor, foreign_key: true, type: :uuid
+      t.integer :vendor_id
       t.string :asset_name
       t.integer :price
       t.string :description_id
-      t.references :company_location, foreign_key: true, type: :uuid
+      t.integer :company_location_id
       t.date :purchase_date
       t.date :warranty_expiry_date
-      t.references :purchase_type, foreign_key: true, type: :uuid
+      t.integer :purchase_type_id
       t.integer :useful_life
       t.integer :residual_value
       t.integer :description
-      t.references :asset_specification, foreign_key: true, type: :uuid
+      t.integer :assest_specification_id
       t.boolean :is_active
 
       t.timestamps
     end
-  end
-
-  def down
-    drop_table :asset_tables
-
-    disable_extension 'pgcrypto' if extension_enabled?('pgcrypto')
   end
 end
