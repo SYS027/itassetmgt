ec4abd7fc085302dea745436fe848c1cd9adc2f8
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-09-29 17:13:14 +0530

    dfdsfs

--- db/migrate/20230929113913_change_country_id_to_integer_in_states.rb
+++ db/migrate/20230929113913_change_country_id_to_integer_in_states.rb
@@ -1,5 +1,5 @@
-class ChangeCountryIdToIntegerInStates < ActiveRecord::Migration[7.0]
+class ChangeCountryIdToIntegerInStates < ActiveRecord::Migration[6.0]
   def change
-    change_column :states, :country_id, :integer
+    change_column :states, :country_id, 'integer USING CAST(country_id AS integer)'
   end
 end
