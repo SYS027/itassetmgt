ef3261667cfa62aebf97c226fdc36c1998938544
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 16:06:40 +0530

    kmkj

--- db/migrate/20231003065424_create_assign_assets.rb
+++ db/migrate/20231003065424_create_assign_assets.rb
@@ -3,16 +3,16 @@ class CreateAssignAssets < ActiveRecord::Migration[7.0]
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
     create_table :assign_assets, id: :uuid do |t|
-      t.references :asset_table, null: false, foreign_key: { to_table: :asset_tables, type: :uuid }
-      t.references :company, null: false, foreign_key: { to_table: :companies, type: :uuid }
-      t.references :product_category, null: false, foreign_key: { to_table: :product_categories}
-      t.references :product_type, null: false, foreign_key: { to_table: :product_types, type: :uuid }
-      t.references :product, null: false, foreign_key: { to_table: :products, type: :uuid }
-      t.references :vendor, null: false, foreign_key: { to_table: :vendors, type: :uuid }
+      t.references :asset_table, null: false, foreign_key: true
+      t.references :company, null: false, foreign_key: true
+      t.references :product_category, null: false, foreign_key: true
+      t.references :product_type, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.references :vendor, null: false, foreign_key: true
       t.string :address
-      t.references :department, null: false, foreign_key: { to_table: :departments, type: :uuid }
-      t.references :employee, null: false, foreign_key: { to_table: :employees, type: :uuid }
-      t.string :description  # Change 'Discription' to 'description'
+      t.references :department, null: false, foreign_key: true
+      t.references :employee, null: false, foreign_key: true
+      t.string :description
       t.string :assign_component
       t.boolean :is_active
 
