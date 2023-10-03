ea00257fe4024e77de832fc5941ea8c2df4e68cd
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 11:30:41 +0530

    aas

--- db/migrate/20230811123112_create_vendors.rb
+++ db/migrate/20230811123112_create_vendors.rb
@@ -2,14 +2,14 @@ class CreateVendors < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
-    create_table :vendors, id: :uuid do |t|
-      t.references :company, null: false, foreign_key: true, type: :uuid
+    create_table :vendors do |t|
+      t.references :company, null: false, foreign_key: true
       t.string :vendor_name
       t.string :email
       t.integer :phone_number
-      t.references :country, null: false, foreign_key: true, type: :uuid
-      t.references :state, null: false, foreign_key: true, type: :uuid
-      t.references :city, null: false, foreign_key: true, type: :uuid
+      t.references :country, null: false, foreign_key: true
+      t.references :state, null: false, foreign_key: true
+      t.references :city, null: false, foreign_key: true
       t.integer :zip_code
       t.string :address
       t.string :description
--- db/migrate/20230811123651_create_location.rb
+++ db/migrate/20230811123651_create_location.rb
@@ -1,14 +1,14 @@
 class CreateLocation < ActiveRecord::Migration[7.0]
 	def change
-	  create_table :locations, id: :uuid do |t|
-		t.references :company, null: false, foreign_key: true, type: :uuid
+	  create_table :locations do |t|
+		t.references :company, null: false, foreign_key: true
 		t.string :office_name
 		t.string :poc_name
 		t.string :poc_email
 		t.string :poc_contact
-		t.references :country, null: false, foreign_key: true, type: :uuid
-		t.references :state, null: false, foreign_key: true, type: :uuid
-		t.references :city, null: false, foreign_key: true, type: :uuid
+		t.references :country, null: false, foreign_key: true
+		t.references :state, null: false, foreign_key: true
+		t.references :city, null: false, foreign_key: true
 		t.string :zip_code
 		t.boolean :is_active
   
