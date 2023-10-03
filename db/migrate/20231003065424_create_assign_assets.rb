373ee522f7cffd42c31c04a7fa7517252b4349f7
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 16:15:24 +0530

    nm

--- db/migrate/20231003065424_create_assign_assets.rb
+++ db/migrate/20231003065424_create_assign_assets.rb
@@ -6,7 +6,7 @@ class CreateAssignAssets < ActiveRecord::Migration[7.0]
       t.references :asset_table, null: false, foreign_key: true
       t.references :company, null: false, foreign_key: true
       t.references :product_category, null: false, foreign_key: true, type: :uuid
-      t.references :product_type, null: false, foreign_key: true
+      t.references :product_type, null: false, foreign_key: true, type: :uuid
       t.references :product, null: false, foreign_key: true
       t.references :vendor, null: false, foreign_key: true
       t.string :address
