9351b0d3b8b3ecbd01d6f3c1ba5a90c92320ad96
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 15:30:16 +0530

    lmk

--- db/migrate/20231003065424_create_assign_assets.rb
+++ db/migrate/20231003065424_create_assign_assets.rb
@@ -2,7 +2,7 @@ class CreateAssignAssets < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
-    create_table :assign_assets do |t|
+    create_table :assign_assets, id: :uuid do |t|
       t.references :asset_table, null: false, foreign_key: { to_table: :asset_tables, type: :uuid }
       t.references :company, null: false, foreign_key: { to_table: :companies, type: :uuid }
       t.references :product_category, null: false, foreign_key: { to_table: :product_categories, type: :uuid }
