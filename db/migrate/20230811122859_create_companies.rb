9195824b1b901c66e903a57f45c3209ccb38c283
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 11:27:00 +0530

    update compaines

--- db/migrate/20230811122859_create_companies.rb
+++ db/migrate/20230811122859_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
-    create_table :companies, id: :uuid do |t|
+    create_table :companies do |t|
       t.string :name
       t.string :email
       t.integer :contact_number
@@ -10,9 +10,9 @@ class CreateCompanies < ActiveRecord::Migration[7.0]
       t.string :industry
       t.integer :number_of_employees
       t.string :tax_information
-      t.references :country, null: false, foreign_key: true, type: :uuid
-      t.references :state, null: false, foreign_key: true, type: :uuid
-      t.references :city, null: false, foreign_key: true, type: :uuid
+      t.references :country, null: false, foreign_key: true
+      t.references :state, null: false, foreign_key: true
+      t.references :city, null: false, foreign_key: true
       t.integer :pin_code
       t.string :address
       t.binary :logo
