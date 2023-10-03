2551aaa695cb195c00d8896c2528c2441baa76c4
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 12:10:59 +0530

    update migration

--- db/migrate/20230811124416_create_notifications.rb
+++ db/migrate/20230811124416_create_notifications.rb
@@ -1,7 +1,7 @@
 class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
-    create_table :notifications do |t|
-      t.references :company, null: false, foreign_key: true
+    create_table :notifications, id: :uuid do |t|
+      t.references :company, null: false, foreign_key: true, type: :uuid
       t.integer :notify_id
       t.string :software_warranty_expiry
       t.string :hardware_warranty_expiry
