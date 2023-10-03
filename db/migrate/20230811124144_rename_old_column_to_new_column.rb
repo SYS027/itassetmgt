021e6b719a291780474728d3ff26ead418dc7d23
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 12:18:26 +0530

    sds

--- db/migrate/20230811124144_rename_old_column_to_new_column.rb
+++ db/migrate/20230811124144_rename_old_column_to_new_column.rb
@@ -1,5 +1,6 @@
 class RenameOldColumnToNewColumn < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :products, :product_catagory_id, :product_category_id
+    def change
+      rename_column :products, :product_catagory_id, :product_category_id
+    end
   end
-end
+  
