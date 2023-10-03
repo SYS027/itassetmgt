4a8fcd409c7ee20a11126785dc68eae89921a468
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 12:17:21 +0530

    dd

--- db/migrate/20230811123921_create_assign_assets.rb
+++ db/migrate/20230811123921_create_assign_assets.rb
@@ -5,13 +5,13 @@ class CreateAssignAssets < ActiveRecord::Migration[7.0]
     create_table :assign_assets do |t|
       t.references :asset_table, null: false, foreign_key: { to_table: :asset_tables }
       t.references :company, null: false, foreign_key: true
-      t.references :product_category, null: false, foreign_key: true
-      t.references :product_type, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
-      t.references :vendor, null: false, foreign_key: true
+      t.references :product_category, null: false, foreign_key: { to_table: :product_categories, type: :uuid }
+      t.references :product_type, null: false, foreign_key: { to_table: :product_types, type: :uuid }
+      t.references :product, null: false, foreign_key: { to_table: :products, type: :uuid }
+      t.references :vendor, null: false, foreign_key: { to_table: :vendors, type: :uuid }
       t.string :address
-      t.references :department, null: false, foreign_key: true, type: :uuid
-      t.references :employee, null: false, foreign_key: true, type: :uuid
+      t.references :department, null: false, foreign_key: { to_table: :departments, type: :uuid }
+      t.references :employee, null: false, foreign_key: { to_table: :employees, type: :uuid }
       t.string :description
       t.string :assign_component
       t.boolean :is_active
