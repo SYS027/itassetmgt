7c77742b5aaaad4e5ce5d3b7d624103a490ed11e
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 14:17:24 +0530

    update changes

--- db/migrate/20230928070439_addtochangecolumn.rb
+++ db/migrate/20230928070439_addtochangecolumn.rb
@@ -1,8 +1,16 @@
 class Addtochangecolumn < ActiveRecord::Migration[7.0]
   def change
-    remove_column :employees, :location
-    remove_column :employees, :department
-    add_reference :employees, :location, foreign_key: true
-    add_reference :employees, :department, foreign_key: true 
+    # Check if the foreign keys exist before removing them
+    if foreign_key_exists?(:employees, :locations)
+      remove_reference :employees, :location, foreign_key: true
+    end
+
+    if foreign_key_exists?(:employees, :departments)
+      remove_reference :employees, :department, foreign_key: true
+    end
+
+    # Add the references with the correct data type
+    add_reference :employees, :location, foreign_key: true, type: :uuid
+    add_reference :employees, :department, foreign_key: true, type: :uuid
   end
 end
