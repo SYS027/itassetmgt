8bfd6f85e13f2e9e6f4c9b54707296cfa5eebeca
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 11:33:32 +0530

    sasasd

--- db/migrate/20230811123541_create_products.rb
+++ db/migrate/20230811123541_create_products.rb
@@ -2,13 +2,13 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
 
-    create_table :products, id: :uuid do |t|
-      t.references :company, null: false, foreign_key: true, type: :uuid
+    create_table :products do |t|
+      t.references :company, null: false, foreign_key: true
       t.integer :product_id
-      t.integer :product_catagory_id
+      t.integer :product_category_id
       t.integer :product_type_id
-      t.string :Product_Name
-      t.string :Manufacturer
+      t.string :product_name
+      t.string :manufacturer
       t.boolean :is_active
 
       t.timestamps
