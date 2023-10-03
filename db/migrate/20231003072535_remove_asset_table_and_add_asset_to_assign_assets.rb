8147a26e3fc94e76188e096205f2fd1070655072
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 16:56:34 +0530

    mnm

--- db/migrate/20231003072535_remove_asset_table_and_add_asset_to_assign_assets.rb
+++ db/migrate/20231003072535_remove_asset_table_and_add_asset_to_assign_assets.rb
@@ -1,15 +1,6 @@
 class RemoveAssetTableAndAddAssetToAssignAssets < ActiveRecord::Migration[7.0]
   def change
-    # Check if the 'asset_table' foreign key exists before removing it
-    if foreign_key_exists?(:assign_assets, :asset_table)
-      # First, remove the existing foreign key constraint
-      remove_reference :assign_assets, :asset_table, foreign_key: true
-    end
-
-    # Add a new 'asset_id' column with the UUID data type
-    add_column :assign_assets, :asset_id, type: :uuid
-
-    # Add a foreign key constraint to the 'assets' table
-    add_foreign_key :assign_assets, :assets, column: :asset_id
+    remove_reference :assign_assets, :asset_table, null: false, foreign_key: true
+    add_reference :assign_assets, :asset, null: false, foreign_key: true
   end
 end
