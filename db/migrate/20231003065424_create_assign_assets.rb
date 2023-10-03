0a61c3c3df2f11a2732413683f9f7d5946f2458d
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-03 15:10:14 +0530

    dddd

--- db/migrate/20231003065424_create_assign_assets.rb
+++ db/migrate/20231003065424_create_assign_assets.rb
@@ -4,11 +4,11 @@ class CreateAssignAssets < ActiveRecord::Migration[7.0]
 
     create_table :assign_assets do |t|
       t.references :asset_table, null: false, foreign_key: { to_table: :asset_tables, type: :uuid }
-      t.references :company, null: false, foreign_key: { to_table: :companies, type: :uuid }
-      t.references :product_category, null: false, foreign_key: { to_table: :product_categories}
-      t.references :product_type, null: false, foreign_key: { to_table: :product_types, type: :uuid }
-      t.references :product, null: false, foreign_key: { to_table: :products, type: :uuid }
-      t.references :vendor, null: false, foreign_key: { to_table: :vendors, type: :uuid }
+      t.references :company, null: false, foreign_key: { to_table: :companies}
+      t.references :id, null: false, foreign_key: { to_table: :product_categories}
+      t.references :id, null: false, foreign_key: { to_table: :product_types, type: :uuid }
+      t.references :product, null: false, foreign_key: { to_table: :products }
+      t.references :vendor, null: false, foreign_key: { to_table: :vendors}
       t.string :address
       t.references :department, null: false, foreign_key: { to_table: :departments, type: :uuid }
       t.references :employee, null: false, foreign_key: { to_table: :employees, type: :uuid }
