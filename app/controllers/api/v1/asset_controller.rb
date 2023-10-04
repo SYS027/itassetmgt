c47a4718d243f91d3f88903bc805bad4a46193f1
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-04 15:40:50 +0530

    again update asset

--- app/controllers/api/v1/asset_controller.rb
+++ app/controllers/api/v1/asset_controller.rb
@@ -1,33 +1,33 @@
 class Api::V1::AssetController < ApplicationController
-  before_action :set_last_company, only: [:create]
-
+ 
   def index
     assets = Asset.all
     assets_array = []
-    assets.each do |asset|
-      hash1 = asset.as_json.symbolize_keys
-      hash2 = {
-        vendor_name: asset.vendor.vendor_name,
-        product_type_name: asset.product_type.product_type,
-        product_name: asset.product.product_name,
-      }
-      new_hash = hash1.merge!(hash2)
-      assets_array.push(new_hash)
-    end
+        assets.each do|asset|
+            hash1 = asset.as_json.symbolize_keys
+            hash2 = {
+                vendor_name: asset.vendor.vendor_name,
+                product_type_name: asset.product_type.product_type,
+                product_name: asset.product.product_name,
+               }
+             new_hash = hash1.merge!(hash2)
+            assets_array.push(new_hash)
+        end
     render json: assets_array
   end
 
   def create
     asset = Asset.new(asset_params)
-    asset.company_id = @last_company.company_id
-    asset.asset_specification_id = nil
+    asset.company_id = Company.last.id
+    asset.asset_specification_id = nil 
     if asset.save
       render json: asset, status: :created
     else
       render json: { errors: asset.errors }, status: :unprocessable_entity
     end
   end
-
+  
+  
   def show
     asset = Asset.find(params[:id])
     render json: asset
@@ -47,16 +47,10 @@ class Api::V1::AssetController < ApplicationController
       render json: { errors: asset.errors }, status: :unprocessable_entity
     end
   end
-
+  
   private
-
   def asset_params
-    params.require(:asset).permit(:product_category_id, :product_type_id, :product_id, :vendor_id, :asset_name, :price, :description, :location_id, :purchase_id, :warranty_expiry_date, :purchase_type_id, :is_active, :serial_number)
-  end
-
-  def set_last_company
-    # Implement logic to set the last company for the user making the request.
-    # For example, if you are using Devise:
-    @last_company = current_user.last_company
+    params.require(:asset).permit(:product_category_id,:product_type_id,:product_id,:vendor_id,:asset_name,:price,:description,:location_id,:purchase_id, :warranty_expiry_date,:purchase_type_id,:is_active, :serial_number)
   end
 end
+
