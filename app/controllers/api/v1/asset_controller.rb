884b8bbb5ed4167f34f9bb3ad9b1846743cdb0f7
SYS027 <133966187+SYS027@users.noreply.github.com>
2023-10-04 15:37:19 +0530

    update asset controller

--- app/controllers/api/v1/asset_controller.rb
+++ app/controllers/api/v1/asset_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AssetController < ApplicationController
-  before_action :set_current_company, only: [:create]
+  before_action :set_last_company, only: [:create]
 
   def index
     assets = Asset.all
@@ -19,7 +19,7 @@ class Api::V1::AssetController < ApplicationController
 
   def create
     asset = Asset.new(asset_params)
-    asset.company_id = @current_company.company_id
+    asset.company_id = @last_company.company_id
     asset.asset_specification_id = nil
     if asset.save
       render json: asset, status: :created
@@ -54,9 +54,9 @@ class Api::V1::AssetController < ApplicationController
     params.require(:asset).permit(:product_category_id, :product_type_id, :product_id, :vendor_id, :asset_name, :price, :description, :location_id, :purchase_id, :warranty_expiry_date, :purchase_type_id, :is_active, :serial_number)
   end
 
-  def set_current_company
-    # Implement logic to set the current company for the user making the request.
+  def set_last_company
+    # Implement logic to set the last company for the user making the request.
     # For example, if you are using Devise:
-    @current_company = current_user.company
+    @last_company = current_user.last_company
   end
 end
